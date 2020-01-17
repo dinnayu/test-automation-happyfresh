f511860e684d4c6d9b8f3b8233c2a4707e60c4d4
dinnayu <dinnayu.k@gmail.com>
2020-01-18 02:43:15 +0700

    add class: - Hook.java - RunTest.java

---
+++ src/test/utility/Hook.java
@@ -0,0 +1,56 @@
+package utility;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Hook {
+	
+	private static String DEVICE_ID = "S8W8QCTCAA9SSCMV";
+	private static WebDriver driver;
+	
+	@Before("@web")
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//drivers//chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@Before("@appium")
+	public void setUpAppium() throws MalformedURLException{
+		
+		// APK file's location
+		File appDir = new File(System.getProperty("user.dir")+"/app/");
+		File app = new File(appDir, "HappyFresh.apk");
+		
+		// Configuration to invoke Appium
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "OPPO F11");
+		cap.setCapability(MobileCapabilityType.UDID, DEVICE_ID);
+		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());s
+		
+		driver = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"), cap);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@After
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	public static WebDriver getDriver(){
+		return driver;
+	}
+}
